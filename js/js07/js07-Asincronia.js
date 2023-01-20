16e02d0b8dfd2b190be9f9c1426b183a1215ff56
Brayan Correa <brayan_seyer@hotmail.com>
2023-01-20 13:00:27 -0600

    feat:added buttons to start and end counter

--- js/js07/js07-Asincronia.js
+++ js/js07/js07-Asincronia.js
@@ -44,3 +44,29 @@ desarrolloAsíncrono(6000); //son segundos
 setIntervalAsincrono(2000);//se repite multiples veces cada 2 segundos o 2000 milisegundos 
 stopIntervalAsincrono();
 despedidad();// esta instruccion no espera a que finalice la funcion asíncrona
+
+
+const refStartInterval = document.getElementById("start-interval");
+const refStopInterval = document.getElementById("stop-interval");
+
+
+let idCounter;
+refStartInterval.addEventListener( 'click', (event) => {
+    let counter = 0;
+    console.log(event);
+    refStartInterval.disabled = true;
+    refStopInterval.disabled = false;
+    idCounter = setInterval( () => console.log(`Contador ${++counter}`),1000);
+
+})
+
+function stopCounter(event){//recibe el evento del boton
+    console.log(event);//se ven las propiedades del evento como clic, mouse touche etc
+    console.log("Se detiene el contador");
+    clearInterval(idCounter)//se pasa el id del contador para que se detenga
+    refStartInterval.disabled = false;
+    refStopInterval.disabled = true
+}
+
+//setTimeOut detiene al llegar a un valor
+// clearinterval detiene cuando el usuario quiere
